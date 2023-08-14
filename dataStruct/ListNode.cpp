6076d2583c48f4b6b094b08378f9a83941c78287
sowrd <15159885771@163.com>
2023-08-14 18:25:20 +0800

    表

--- dataStruct/ListNode.cpp
+++ dataStruct/ListNode.cpp
@@ -1,7 +1,55 @@
-//
-// Created by LENOVO on 2023/8/8.
-//
 
-// 链表实现--不带头节点
+
+#include <stdio.h>
+#include <stdlib.h>
+// 链表的实现及其操作
+
+// 定义链表-单链表
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+    ListNode* reverseList(ListNode* head) {
+
+        ListNode* newHead =nullptr;
+        //ListNode*  =head;
+        ListNode* temp = nullptr;
+
+
+        if(head ==nullptr || head->next == nullptr){
+
+            return head;
+        }
+
+        while(head){
+
+            temp = head->next;
+            head->next = newHead;
+            newHead = head;
+            head = temp;
+        }
+        return newHead;
+    }
+
+    // 尾插法建立单链表
+    ListNode* InsertList(ListNode* head){
+
+        ListNode* r= head;
+        ListNode* s= head;
+
+        while (head){
+
+            r->next= s;
+            r=s;
+        }
+        r->next= nullptr;
+        return head;
+
+    }
 
 
