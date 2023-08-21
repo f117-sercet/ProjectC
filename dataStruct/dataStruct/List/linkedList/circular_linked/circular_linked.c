3e5f92655552091ecd3da11687ed3c4d78312af9
sowrd <15159885771@163.com>
2023-08-21 18:27:47 +0800

    链表-循环链表

---
+++ dataStruct/dataStruct/List/linkedList/circular_linked/circular_linked.c
@@ -0,0 +1,35 @@
+//
+// Created by LENOVO on 2023/8/17.
+//
+
+#include  <stdio.h>
+#include <conio.h>
+#include  <stdlib.h>
+#define  NULL  0
+
+
+typedef struct node {
+    int data;
+    struct node *next;
+
+} ListNode;
+
+ListNode  *first = NULL;
+ListNode  *last=NULL ;
+
+void create() {
+
+    int i,n;
+
+    printf("Enter the number of nodes required:\n") ;
+    scanf("%d",&n) ;
+
+    for(i=1; i<n; i++){
+
+
+    }
+
+}
+
+
+
--- dataStruct/dataStruct/List/linkedList/circular_linked/circular_linked.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by LENOVO on 2023/8/17.
-//
-
-#include  <stdio.h>
-#include <conio.h>
-#include  <stdlib.h>
-
-
