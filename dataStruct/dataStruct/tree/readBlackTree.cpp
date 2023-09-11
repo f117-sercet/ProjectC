60ffce5e5291e7b5d8b25a1cc2d5e4855dbfc9c4
sowrd <15159885771@163.com>
2023-09-11 10:28:06 +0800

    红黑树完善

--- dataStruct/dataStruct/tree/readBlackTree.cpp
+++ dataStruct/dataStruct/tree/readBlackTree.cpp
@@ -5,8 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node
-{
+typedef struct node {
     int val;
     struct node *par;
     struct node *left;
@@ -15,18 +14,16 @@ typedef struct node
 } Node;
 
 
-Node *newNode(int val,Node *par)
-{
-Node *node = new Node;
- node->val =val;
- node->par = par;
- node->left = NULL;
- node->right = nullptr;
- node->color = 1;
+Node *newNode(int val, Node *par) {
+    Node *node = new Node;
+    node->val = val;
+    node->par = par;
+    node->left = NULL;
+    node->right = nullptr;
+    node->color = 1;
 }
 
-int isLeafNode(Node *node)
-{
+int isLeafNode(Node *node) {
     if (node->left == nullptr && node->right == nullptr)
         return 1;
     return 0;
@@ -34,27 +31,21 @@ int isLeafNode(Node *node)
 
 // 左旋树
 
-Node *leftRotate(Node *node)
-{
+Node *leftRotate(Node *node) {
     Node *parent = node->par;
     Node *grandParent = parent->par;
 
     parent->right = node->left;
-    if (node->left != NULL)
-    {
+    if (node->left != NULL) {
         node->left->par = parent;
     }
     node->par = grandParent;
     parent->par = node;
     node->left = parent;
-    if (grandParent != NULL)
-    {
-        if (grandParent->right == parent)
-        {
+    if (grandParent != NULL) {
+        if (grandParent->right == parent) {
             grandParent->right = node;
-        }
-        else
-        {
+        } else {
             grandParent->left = node;
         }
     }
@@ -62,32 +53,27 @@ Node *leftRotate(Node *node)
 }
 
 // Right Rotate
-Node *rightRotate(Node *node)
-{
+Node *rightRotate(Node *node) {
     Node *parent = node->par;
     Node *grandParent = parent->par;
 
     parent->right = node->left;
-    if (node->left != NULL)
-    {
+    if (node->left != NULL) {
         node->left->par = parent;
     }
     node->par = grandParent;
     parent->par = node;
     node->left = parent;
-    if (grandParent != nullptr)
-    {
-        if (grandParent->right == parent)
-        {
+    if (grandParent != nullptr) {
+        if (grandParent->right == parent) {
             grandParent->right = node;
-        }
-        else
-        {
+        } else {
             grandParent->left = node;
         }
     }
     return node;
 }
+
 void checkNode(Node *node) {
     if (node == nullptr || node->par == nullptr) {
 
@@ -125,147 +111,134 @@ void checkNode(Node *node) {
         // 左旋
         Node *greatGrandParent = grandParent->par;
 
-        if (grandParent->right ==parent)
-        {
+        if (grandParent->right == parent) {
 
 
 
-        // 右边的情况
-        if (grandParent->right == node) {
-            grandParent->right = parent->left;
+            // 右边的情况
+            if (grandParent->right == node) {
+                grandParent->right = parent->left;
 
-            if (parent->left != nullptr) {
-                (parent->left)->par = grandParent;
-            }
-            parent->left = grandParent;
-            grandParent->par = parent;
-
-            //Attach to existing Tree
-            parent->par = greatGrandParent;
-            if (greatGrandParent != nullptr) {
-                if (greatGrandParent->left != nullptr
-                    && greatGrandParent->left == grandParent) {
-
-                    greatGrandParent->left = parent;
-                } else {
-                    greatGrandParent->right = parent;
+                if (parent->left != nullptr) {
+                    (parent->left)->par = grandParent;
                 }
-            }
-            // 改变颜色
-            parent->color = 0;
-            grandParent->color = 1;
+                parent->left = grandParent;
+                grandParent->par = parent;
 
+                //Attach to existing Tree
+                parent->par = greatGrandParent;
+                if (greatGrandParent != nullptr) {
+                    if (greatGrandParent->left != nullptr
+                        && greatGrandParent->left == grandParent) {
+
+                        greatGrandParent->left = parent;
+                    } else {
+                        greatGrandParent->right = parent;
+                    }
+                }
+                // 改变颜色
+                parent->color = 0;
+                grandParent->color = 1;
+
+            } else {
+                // 右子树左子树情况
+                //儿子左旋
+                parent->left = child->right;
+                if (child->right != nullptr) {
+                    (child->right)->par = parent;
+                }
+                child->right = parent;
+                parent->par = child;
+
+                // 第二步:儿子 和祖父旋转
+                grandParent->right = child->left;
+                if (child->left != nullptr) {
+                    (child->left)->par = grandParent;
+                }
+                child->left = grandParent;
+                grandParent->par = child;
+
+                // Attach to the existing tree
+                child->par = greatGrandParent;
+                if (greatGrandParent != nullptr) {
+                    if (greatGrandParent->left != nullptr &&
+                        greatGrandParent->left == grandParent) {
+                        greatGrandParent->left = child;
+                    } else {
+                        grandParent->right = child;
+                    }
+                }
+                // 改变颜色
+                child->color = 0;
+                grandParent->color = 1;
+            }
         } else {
-            // 右子树左子树情况
-            //儿子左旋
-            parent->left = child->right;
-            if (child->right != nullptr) {
-                (child->right)->par = parent;
-            }
-            child->right = parent;
-            parent->par = child;
+            //left Case
+            // Left Left Case
 
-            // 第二步:儿子 和祖父旋转
-            grandParent->right = child->left;
-            if (child->left != nullptr) {
-                (child->left)->par = grandParent;
-            }
-            child->left = grandParent;
-            grandParent->par = child;
+            if (parent->left == node) {
 
-            // Attach to the existing tree
-            child->par = greatGrandParent;
-            if (greatGrandParent != nullptr) {
-                if (greatGrandParent->left != nullptr &&
-                    greatGrandParent->left == grandParent) {
-                    greatGrandParent->left = child;
-                } else {
-                    grandParent->right = child;
+                grandParent->left = parent->right;
+                if (parent->right != nullptr) {
+                    (parent->right)->par = grandParent;
                 }
-            }
-            // 改变颜色
-            child->color = 0;
-            grandParent->color = 1;
-        }
-    }
-    else
-    {
-        //left Case
-        // Left Left Case
+                parent->par = greatGrandParent;
+                grandParent->par = parent;
 
-        if (parent->left == node){
+                // Attach to existing trees
 
-            grandParent->left = parent->right;
-            if (parent->right != nullptr)
-            {
-                (parent->right)->par = grandParent;
-            }
-            parent->par = greatGrandParent;
-            grandParent->par = parent;
-
-            // Attach to existing trees
-
-            parent->par = greatGrandParent;
-            if (greatGrandParent != nullptr)
-            {
-                if (greatGrandParent->left != nullptr
-                && grandParent->left == grandParent){
-                    grandParent->left = parent;
+                parent->par = greatGrandParent;
+                if (greatGrandParent != nullptr) {
+                    if (greatGrandParent->left != nullptr
+                        && grandParent->left == grandParent) {
+                        grandParent->left = parent;
+                    } else {
+                        greatGrandParent->right = parent;
+                    }
                 }
-                else
-                {
-                    greatGrandParent->right = parent;
-                }
+
+                // 换色
+                parent->color = 0;
+                grandParent->color = 1;
+            } else {
+                // Left Right Case
+
+                // First step->Parent Child Rotation
+                parent->right = child->left;
+
+
             }
-
-             // 换色
-             parent->color = 0;
-            grandParent->color = 1;
         }
-        else
-        {
-            // Left Right Case
-
-            // First step->Parent Child Rotation
-            parent->right =child->left;
-
-
-        }
-    }
     }
 }
 
-void insertNode(int val,Node **root){
+void insertNode(int val, Node **root) {
 
     Node *buffRoot = *root;
-    while (buffRoot){
+    while (buffRoot) {
 
-        if (buffRoot->val>val){
+        if (buffRoot->val > val) {
 
-            if (buffRoot->val>val){
+            if (buffRoot->val > val) {
 
-                if (buffRoot->left != nullptr){
+                if (buffRoot->left != nullptr) {
                     buffRoot = buffRoot->left;
 
-                } else
-                {
-                   Node *toInsert =  newNode(val,buffRoot);
-                  buffRoot->left = toInsert;
-                  buffRoot = toInsert;
+                } else {
+                    Node *toInsert = newNode(val, buffRoot);
+                    buffRoot->left = toInsert;
+                    buffRoot = toInsert;
 
                     break;
                 }
 
-            }
-            else
-            {
-                if (buffRoot->right != nullptr){
+            } else {
+                if (buffRoot->right != nullptr) {
 
                     buffRoot = buffRoot->right;
-                }
-                else{
+                } else {
 
-                    Node *toInsert = newNode(val,buffRoot);
+                    Node *toInsert = newNode(val, buffRoot);
                     buffRoot->right = toInsert;
                     buffRoot = toInsert;
 
@@ -273,29 +246,209 @@ void insertNode(int val,Node **root){
                 }
             }
         }
-        while (buffRoot !=*root)
-        {
+        while (buffRoot != *root) {
             checkNode(buffRoot);
-            if (buffRoot->par == nullptr){
+            if (buffRoot->par == nullptr) {
 
                 *root = buffRoot;
                 break;
             }
             buffRoot = buffRoot->par;
 
-            if (buffRoot ==*root)
-            {
+            if (buffRoot == *root) {
                 buffRoot->color = 0;
             }
         }
     }
-   void checkForCase2(Node *toDelete, int del, int fromDirection, Node **root){
+}
 
-       if (toDelete == (*root))
-       {
-           (*root)->color = 0;
-           return;
-       }
+void checkForCase2(Node *toDelete, int deletes, int fromDirection, Node **root) {
+    if (toDelete == (*root)) {
+        (*root)->color = 0;
+        return;
+    }
+
+    if (!deletes && toDelete->color == 1) {
+        if (!fromDirection) {
+            if (toDelete->right != NULL) {
+                toDelete->right->color = 1;
+            }
+        } else {
+            if (toDelete->left != NULL) {
+                toDelete->left->color = 1;
+            }
+        }
+        toDelete->color = 0;
+        return;
+    }
+
+    Node *sibling;
+    Node *parent = toDelete->par;
+    int locateChild = 0;
+
+    if (parent->right == toDelete) {
+
+        sibling = parent->left;
+        locateChild = 1;
+
+    } else {
+        sibling = parent->right;
+    }
+
+    if ((sibling->right != nullptr && sibling->right->color == 1)
+        || (sibling->left != nullptr && sibling->left->color == 1)) {
+
+        if (sibling->right != nullptr && sibling->right->color == 1) {
+            if (locateChild == 1) {
+
+                int parColor = parent->color;
+                sibling = leftRotate(sibling->right);
+                parent = rightRotate(sibling);
+
+                if (parent->par == nullptr) {
+
+                    *root = parent;
+                }
+
+                parent->color = parColor;
+                parent->left->color = 0;
+
+                if (deletes) {
+                    if (toDelete->left != nullptr) {
+
+                        toDelete->left->par = parent->right;
+                    }
+                    parent->right->right = toDelete->left;
+                    free(toDelete);
+                }
+
+            } else {
+                int parColor = parent->color;
+
+                parent = leftRotate(sibling);
+
+                if (parent->par == nullptr) {
+                    *root = parent;
+                }
+
+
+                parent->color = parColor;
+                parent->left->color = 0;
+                parent->right->color = 0;
+
+                if (deletes) {
+
+                    if (toDelete->right != nullptr) {
+
+                        toDelete->right->par = parent->left;
+                    }
+
+                    parent->left->left = toDelete->left;
+                    free(toDelete);
+                }
+            }
+        } else {
+
+            if (locateChild == 0) {
+                int parColor = parent->color;
+
+                sibling = rightRotate(sibling->left);
+                parent = leftRotate(sibling);
+
+                if (parent->par == nullptr) {
+
+                    *root = parent;
+                }
+                parent->color = parColor;
+                parent->left->color = 0;
+                parent->right->color = 0;
+
+                if (deletes) {
+                    if (toDelete->right != nullptr) {
+
+                        toDelete->right->par = parent->left;
+                    }
+                    parent->left->left = toDelete->right;
+                    free(toDelete);
+                }
+            } else {
+                int parColor = parent->color;
+                parent = rightRotate(sibling);
+
+                if (parent->par == nullptr) {
+                    *root = parent;
+                }
+
+
+                parent->color = parColor;
+                parent->left->color = 0;
+                parent->right->color;
+
+                if (deletes) {
+                    if (toDelete->left != nullptr) {
+                        toDelete->left->par = parent->right;
+                    }
+                    parent->right->right = toDelete->left;
+                    free(toDelete);
+                }
+            }
+        }
+    } else if (sibling->color == 0) {
+
+        sibling->color = 1;
+
+        if (deletes) {
+
+
+            if (locateChild) {
+                toDelete->par->right = toDelete->left;
+
+                if (toDelete->left != nullptr) {
+                    toDelete->left->par = toDelete->par;
+                }
+            } else {
+                toDelete->par->left = toDelete->right;
+
+                if (toDelete->right != nullptr) {
+                    toDelete->right->par = toDelete->par;
+                }
+            }
+        } else {
+            if (locateChild) {
+                toDelete->par->right = toDelete->left;
+                if (toDelete->left != nullptr) {
+
+                    toDelete->left->par = toDelete->par;
+                }
+                parent = rightRotate(sibling);
+
+                if (parent->par == nullptr) {
+
+                    *root = parent;
+                }
+                parent->color = 0;
+                parent->right->color = 1;
+                checkForCase2(parent->right, 0, 1, root);
+            } else {
+
+
+                // 左旋
+                toDelete->par->left = toDelete->right;
+
+                if (toDelete->right != nullptr) {
+                    toDelete->right->par = toDelete->par;
+                }
+                parent = leftRotate(sibling);
+
+                if (parent->par == nullptr) {
+                    *root = parent;
+                }
+                printf("\nroot - %d - %d\n", parent->val, parent->left->val);
+                parent->color = 0;
+                parent->left->color = 1;
+                checkForCase2(parent->left, 0, 0, root);
+            }
+        }
     }
 }
 
